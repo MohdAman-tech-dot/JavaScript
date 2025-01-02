8241a54a82cbbaac5a085be2ee8f6b799e26170e
MohdAman-tech-dot <mohdaman84410@gmail.com>
2025-01-02 11:52:30 +0530

    first

---
+++ 1-J.S Intro.js
@@ -0,0 +1,15 @@
+alert("Error 106")
+
+console.log("Code is running...")
+
+var a = prompt ("Enter your number")
+console.log("Your number is: " + a)
+
+var isTrue = confirm("Are you sure about that you want to blast your system")
+
+if(isTrue) {
+    console.log("Your computer is blasting")
+}
+else {
+    console.log("Your system is not blasting")
+}
