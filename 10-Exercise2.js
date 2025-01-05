77efb0251165a1ce28c85aae7b187e1acfe6b9dc
MohdAman-tech-dot <mohdaman84410@gmail.com>
2025-01-05 22:05:14 +0530

    Exericse-2

---
+++ 10-Exercise2.js
@@ -0,0 +1,15 @@
+function nameGenerator() {
+     // Strings of words separated by spaces
+    let adjectives = "Crazy Amazing Fire";
+    let nouns = "Engine Foods Garments";
+    let extras = "Limited Bros Hub";
+
+    // Convert string to individual words using split(" ")
+    let adj = adjectives.split(" ")[Math.floor(Math.random() * 3)];
+    let noun = nouns.split(" ")[Math.floor(Math.random() * 3)];
+    let extra = extras.split(" ")[Math.floor(Math.random() * 3)];
+
+    return `${adj} ${noun} ${extra}`;
+}
+
+console.log(nameGenerator());
