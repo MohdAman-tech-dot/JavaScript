6ee911b8a90197c7a0a4222c1f8ea2ad9114facd
MohdAman-tech-dot <mohdaman84410@gmail.com>
2025-01-11 18:50:09 +0530

    array

---
+++ 11-ArraysLoops.js
@@ -0,0 +1,67 @@
+//1. for loop
+let a = [101,14,52,78,63]
+for (let index=0; index<a.length; index++) {
+    console.log("I:-",a[index])
+}
+
+// 2. for each loop
+const b = ["mohd",13,"aman",5,"choudhar",]
+b.forEach((value, index, array) => {
+    console.log("II:-","Value:",value,  "index:", index,  "Array:",array)
+})
+
+// 3. for in loop
+let obj = {
+    1: "a",
+    2: "b",
+    3: "c"
+}
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {   //build in properties chodke khud ki properties hee chahiye isliye "hasown" use hota h
+        const element = obj[key];
+        console.log("III:-",key, element)
+    }
+}
+
+// 4. for of loop
+let p = ["mohd aiyyoob", "mohd aman", "mohd ajaz"]
+for (const element of p) {
+    console.log("IV:-",element)
+}
+
+// performing square using for loop
+let A = [8,5,9,6]
+let B =[]
+for (let index = 0; index < A.length; index++) {
+    B.push(A[index]**2)
+}
+console.log("try:-",B)
+
+// 5. map() :- creates a new array by performing some operation on each element
+let q = [5,3,9,4,6]
+let newArr = q.map((value, index, array)=>{
+    return value**3     //power 3 :- q array's each multiply by 3 times with self 
+})
+console.log("V:-",newArr)
+
+// 6. filter() :- filters an array with values that passes a test, creates a new array
+let r = [5,8,9,6,7,3]
+
+console.log("VI:-",r.filter(checkNum))
+
+function checkNum(no) {
+    return no>6
+}
+
+// 7. reduce() :- (apne next element se fer next fer next continues operation perform krega)
+                    //EX- [2,5,6]=2*5=10 fer 10*6=60
+let Arr = [5,2,3,6,7]
+
+const mul = (a,b) => {
+    return a*b
+}
+console.log("VII-Multiply:",Arr.reduce(mul))
+
+// 8. Array.from :- used to create an array from any other object
+let name = Array.from("Aman")
+console.log(name)
--- 7-functions.js
+++ 7-functions.js
@@ -5,24 +5,24 @@ sub(7, 1)
 
 // use for chage the name to many lines
 function name(a) {
-    console.log("Hey," + a + "Never gone lose hope and never give up")
-    console.log("Hey," + a + "You are the best developer")
-    console.log("Hey," + a + "Always be polite with everyone")
-    console.log("Hey," + a + "Never show attitude")
+    console.log("Hey," + a + "Never gone lose hope and never give up.")
+    console.log("Hey," + a + "You are the best developer.")
+    console.log("Hey," + a + "Always be polite with everyone.")
+    console.log("Hey," + a + "Never show attitude.")
 }
 name(" 'Mohd Aman Choudhary'- ")
 
 // function with return
 function sum(x, y, z = 2) {
-    return x + y + z
+    return x + y 
 }
 result1 = sum(9, 2)
 result2 = sum(4, 1)
 result3 = sum(7, 5)
 
-console.log("Substraction=", result1)
-console.log("Substraction=", result2)
-console.log("Substraction=", result3)
+console.log("Addition=", result1)
+console.log("Addition=", result2)
+console.log("Addition=", result3)
 
 // Arrow function
 const func1 = (x) => {
